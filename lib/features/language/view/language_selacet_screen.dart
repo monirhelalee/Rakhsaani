9aca94278898829b57d13c5367db70987f22bdb2
monir <mdmonirhelale@gmail.com>
2022-10-27 23:13:29 +0600

    add language select screen

---
+++ lib/features/language/view/language_selacet_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageSelectScreen extends StatefulWidget {
+  const LanguageSelectScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LanguageSelectScreen> createState() => _LanguageSelectScreenState();
+}
+
+class _LanguageSelectScreenState extends State<LanguageSelectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
