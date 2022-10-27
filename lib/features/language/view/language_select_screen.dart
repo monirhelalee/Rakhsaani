0a8ecec1bcdef5eb24d8e9d25b751330d0d1d19c
whoissharif <sharif.csebubt@gmail.com>
2022-10-28 00:10:24 +0600

    language select basic skeleton

--- lib/features/language/view/language_select_screen.dart
+++ lib/features/language/view/language_select_screen.dart
@@ -10,6 +10,57 @@ class LanguageSelectScreen extends StatefulWidget {
 class _LanguageSelectScreenState extends State<LanguageSelectScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const FlutterLogo(),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              'Al-Quran',
+              style: TextStyle(fontSize: 35),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            const Text(
+              'Select Language',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Card(
+                child: Column(
+                  children: ['Bengali', 'English', 'Spanish']
+                      .map(
+                        (e) => ListTile(
+                          title: Text(e),
+                          trailing: Radio(
+                            value: false,
+                            groupValue: '',
+                            onChanged: (s) {},
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
