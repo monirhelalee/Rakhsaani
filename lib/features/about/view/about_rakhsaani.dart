6f7e390f95032c1d144aa141a32455a114d62887
monir <mdmonirhelale@gmail.com>
2022-11-05 00:10:51 +0600

    about done

--- lib/features/about/view/about_rakhsaani.dart
+++ lib/features/about/view/about_rakhsaani.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rakhsaani/core/utils/colors.dart';
+import 'package:rakhsaani/core/utils/constant.dart';
+import 'package:rakhsaani/core/utils/styles.dart';
 import 'package:rakhsaani/core/widgets/common_text_widget.dart';
 
 class AboutRakhsaani extends StatelessWidget {
@@ -6,9 +9,86 @@ class AboutRakhsaani extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
-        title: CommonTextWidget("About"),
+        title: CommonTextWidget("About", style: AppTextStyles.title20),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight / 2.3,
+            width: screenWidth,
+            padding: const EdgeInsets.all(30),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background_image_about.jpg"),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CommonTextWidget(
+                  "My Lord,",
+                  style: AppTextStyles.title40,
+                ),
+                CommonTextWidget(
+                  "Increase Me In",
+                  style: AppTextStyles.title29.copyWith(fontSize: 24),
+                ),
+                CommonTextWidget(
+                  "Knowledge.",
+                  style: AppTextStyles.title29.copyWith(fontSize: 30),
+                ),
+                spaceBetween,
+                Row(
+                  children: [
+                    spaceAround,
+                    spaceAround,
+                    spaceAround,
+                    spaceAround,
+                    CommonTextWidget(
+                      " - Surah Taha(20:14)",
+                      style: AppTextStyles.regular12,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          spaceBetween,
+          spaceBetween,
+          spaceBetween,
+          Column(
+            children: [
+              CommonTextWidget(
+                "About Rakhasaani Trust",
+                style: AppTextStyles.regular18.copyWith(fontSize: 24),
+              ),
+              spaceBetween,
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: CommonTextWidget(
+                  "To disseminate the message of The Holy Quran to diverse people irrespective of race, culture, religion, muslim/ non-muslim crosswise the global in their native language for the clear understanding of intense and accurate meaning of Quran and simultaneously promote harmonious relations for communal peace and goodwill so that people can recognize the dissimilar aspects of it and resolve any conflicts that aries by establishing goodness and virtues.",
+                  textAlign: TextAlign.justify,
+                  style: AppTextStyles.regular16,
+                ),
+              ),
+              spaceBetween,
+              spaceBetween,
+              spaceBetween,
+              CommonTextWidget(
+                "www.rakhsaanitrust.org",
+                style: AppTextStyles.regular14,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
